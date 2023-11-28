c8385787f2810a7e252bc38b42d7e3e4791603d0
lteapot293 <123525317+lteapot293@users.noreply.github.com>
2023-11-28 19:29:39 +0300

    Delete dunst directory

--- dunst/scripts/show_volume.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-volume=$(pactl get-sink-volume @DEFAULT_SINK@ | awk -F '/' '{print substr($2, 0, length($2)-2)}' | awk '{$1=$1;print}')
-notify-send -t 600 "Volume" -h int:value:"${volume}"
